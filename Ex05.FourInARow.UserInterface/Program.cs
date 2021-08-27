577a33e208affb4cbd815d80d3e3231a386dffe2
OrSamu <orfacemailj@gmail.com>
2021-08-27 13:02:01 +0300

    first commit

---
+++ Ex05.FourInARow.UserInterface/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Forms;
+
+namespace Ex05.FourInARow.UserInterface
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            FormSettings test = new FormSettings();
+
+            test.ShowDialog();
+        }
+    }
+}
---
+++ Ex05.FourInARow.UserInterface/UIManager.cs
@@ -0,0 +1,15 @@
+﻿namespace Ex05.FourInARow.UserInterface
+{
+    public class UIManager
+    {
+        public static void Start()
+        {
+            int rows, cols;
+            bool vsComputer;
+
+            //get settings
+            //play game
+            //byebye
+        }
+    }
+}
